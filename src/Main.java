88f3512bb3aa86e76519459c69e9216eb6ac78e9
jpinaz92 <jpinaz92@gmail.com>
2015-10-14 20:52:12 +1100

    Replication 3
    code slightly modified to simulate 100 games used to replicate the odds
    bias bug

--- src/Main.java
+++ src/Main.java
@@ -24,7 +24,7 @@ public class Main {
             int winCount = 0;
             int loseCount = 0;
             
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 100; i++)
             {
             	String name = "Fred";
             	int balance = 100;
